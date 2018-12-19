78b6f1b5697cca98001c99c7f0fd64c20dd3b0b0
Hury <mashurimansur18@gmail.com>
2018-12-19 12:59:48 +0800

    membuat dolder khusus model

---
+++ app/Models/Buku.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    protected $table = 'buku';
+}
---
+++ app/Models/Transaksi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    protected $table = 'transaksi';
+    protected $fillable = ['id', 'id_buku', 'id_user', 'tgl_pinjam', 'tgl_kembali', 'created_at', 'updated_at'];
+    protected $hidden = ['created_at', 'updated_at', 'id_user', 'id_buku'];
+
+    public function buku() {
+        return $this->belongsTo('App\Models\Buku', 'id_buku');
+    }
+
+    public function user() {
+        return $this->belongsTo('App\User', 'id_user');
+    }
+}
