67911973c3d4e5d5f78acf50714aa7364427b684
Hury <mashurimansur18@gmail.com>
2018-12-20 17:07:49 +0800

    mengubah tampilan form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,23 +3,15 @@
 <head>
 	<title>Login</title>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<link rel="icon" type="image/png" href="{{ asset('assets') }}/login/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/login/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/login/vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/login/vendor/animate/animate.css">	
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/login/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/login/vendor/select2/select2.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/login/css/util.css">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/login/css/main.css">
-<!--===============================================================================================-->
 </head>
 <body>
 	
@@ -78,8 +70,8 @@
 						</a>
 					</div>
 
-					<div class="text-center p-t-136">
-                    <a class="txt2" href="{{ route('register') }}">
+					<div class="text-center p-t-70">
+                  		<a class="txt2" href="{{ route('register') }}">
 							Create your Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
@@ -88,25 +80,17 @@
 			</div>
 		</div>
 	</div>
-	
-	
-
-	
-<!--===============================================================================================-->	
+		
 	<script src="{{ asset('assets') }}/login/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
 	<script src="{{ asset('assets') }}/login/vendor/bootstrap/js/popper.js"></script>
 	<script src="{{ asset('assets') }}/login/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
 	<script src="{{ asset('assets') }}/login/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
 	<script src="{{ asset('assets') }}/login/vendor/tilt/tilt.jquery.min.js"></script>
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-<!--===============================================================================================-->
 	<script src="{{ asset('assets') }}/login/js/main.js"></script>
 
 </body>
